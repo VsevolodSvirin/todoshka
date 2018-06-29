91a6d2a5731c21f89b7f9a2ff68e296c71ebac5a
VsevolodSvirin <vsvirin@smedialink.com>
2018-06-29 15:45:47 +0300

    Refactor

--- api/permissions.py
+++ api/permissions.py
@@ -3,9 +3,7 @@ from rest_framework.permissions import BasePermission
 
 class IsAuthorOrAdmin(BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request.user.is_superuser:
-            return True
-        return obj.pk == request.user.pk
+        return request.user.is_superuser or obj.pk == request.user.pk
 
     def has_permission(self, request, view):
         return request.user.is_authenticated
