aa2751277d5045390673cc1c059becca59c8c561
VsevolodSvirin <vsvirin@smedialink.com>
2018-07-18 09:06:52 +0300

    Add user permissions

---
+++ users/tests/test_permissions.py
@@ -0,0 +1,35 @@
+from unittest.mock import Mock
+
+from django.test import TestCase
+
+from users.models import User
+from users.permissions import IsSelfOrAdmin
+
+
+class PermissionsTestCase(TestCase):
+    def setUp(self):
+        self.mock_request = Mock()
+        self.permission = IsSelfOrAdmin()
+
+        self.user = User.objects.create_user(username='Awesome Bob', email='awesome@bob.com', password='123')
+        self.user_2 = User.objects.create_user(username='Magnificent Jane', email='magnificent@jane.com',
+                                               password='123')
+        self.admin = User.objects.create_superuser(username='Reckless Joe', email='lul@olo.lo', password='12345678')
+
+    def test_admin_can_edit_everybody(self):
+        self.mock_request.user = self.admin
+        response = self.permission.has_object_permission(self.mock_request, None, self.user)
+
+        self.assertTrue(response)
+
+    def test_user_can_edit_himself(self):
+        self.mock_request.user = self.user
+        response = self.permission.has_object_permission(self.mock_request, None, self.user)
+
+        self.assertTrue(response)
+
+    def test_user_cannot_edit_others(self):
+        self.mock_request.user = self.user_2
+        response = self.permission.has_object_permission(self.mock_request, None, self.user)
+
+        self.assertFalse(response)
