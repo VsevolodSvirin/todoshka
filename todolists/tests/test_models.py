d0758e5eaf318363cf24afdbf16f64ddf98b770e
VsevolodSvirin <vsvirin@smedialink.com>
2018-07-25 09:50:22 +0300

    Update model tests

--- todolists/tests/test_models.py
+++ todolists/tests/test_models.py
@@ -8,9 +8,7 @@ User = get_user_model()
 
 
 class TodoListTestCase(TestCase):
-
-    # TODO research if more tests needed
-    def test_create_todo_list(self):
+    def test_create_todo_list_success(self):
         user = User.objects.create_user(username='Cool Guy',
                                         email='cool_guy@smedilink.com',
                                         password='123')
--- users/tests/test_models.py
+++ users/tests/test_models.py
@@ -5,7 +5,7 @@ from users.models import User
 
 
 class UserTestCase(TestCase):
-    def test_create_user(self):
+    def test_create_user_success(self):
         old_count = User.objects.count()
 
         User.objects.create_user(username='Cool Guy',
@@ -15,7 +15,19 @@ class UserTestCase(TestCase):
 
         self.assertEqual(old_count + 1, new_count)
 
-    def test_no_email(self):
+    def test_bad_username_fail(self):
         with self.assertRaises(ValidationError):
-            User.objects.create_user(username='Not So Cool Guy',
+            User.objects.create_user(username='(C)ool Gu¥',
+                                     email='cool_guy@smedilink.com',
                                      password='123')
+
+    def test_missing_fields_fail(self):
+        with self.assertRaises(ValidationError):
+            User.objects.create_user(username='Bad Guy',
+                                     email='bad_guy@smedilink.com')
+
+            User.objects.create_user(username='Bad Guy',
+                                     password='321')
+
+            User.objects.create_user(email='bad_guy@smedilink.com',
+                                     password='321')
