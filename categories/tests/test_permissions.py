51ff4fa58b0f87a3716f0df1cbd2d518ce65621d
VsevolodSvirin <vsvirin@smedialink.com>
2018-07-31 14:39:39 +0300

    Create common and category permissions, init category views

--- categories/tests/test_permissions.py
+++ categories/tests/test_permissions.py
@@ -3,9 +3,9 @@ from unittest.mock import Mock
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-from todolists.models import TodoList
+from categories.models import Category
+from categories.views import CategoryViewSet
 from todolists.permissions import IsAuthorOrAdmin
-from todolists.views import TodoListViewSet
 
 User = get_user_model()
 
@@ -13,7 +13,7 @@ User = get_user_model()
 class PermissionsTestCase(TestCase):
     def setUp(self):
         self.mock_request = Mock()
-        self.view = TodoListViewSet()
+        self.view = CategoryViewSet()
 
         self.permission = IsAuthorOrAdmin()
 
@@ -27,29 +27,29 @@ class PermissionsTestCase(TestCase):
                                                    email='reckless@joe.com',
                                                    password='12345678')
 
-        self.user_todo_list = TodoList.objects.create(name='Bobby\'s List',
-                                                      author=self.user)
+        self.user_category = Category.objects.create(name='Bobby\'s Category',
+                                                     user=self.user)
 
     def tearDown(self):
         self.user.delete()
         self.user_2.delete()
         self.admin.delete()
-        self.user_todo_list.delete()
+        self.user_category.delete()
 
     def test_admin_can_edit_everything(self):
         self.mock_request.user = self.admin
-        response = self.permission.has_object_permission(self.mock_request, self.view, self.user_todo_list)
+        response = self.permission.has_object_permission(self.mock_request, self.view, self.user_category)
 
         self.assertTrue(response)
 
     def test_user_can_edit_his_list(self):
         self.mock_request.user = self.user
-        response = self.permission.has_object_permission(self.mock_request, self.view, self.user_todo_list)
+        response = self.permission.has_object_permission(self.mock_request, self.view, self.user_category)
 
         self.assertTrue(response)
 
     def test_user_cannot_edit_list_of_another_user(self):
         self.mock_request.user = self.user_2
-        response = self.permission.has_object_permission(self.mock_request, self.view, self.user_todo_list)
+        response = self.permission.has_object_permission(self.mock_request, self.view, self.user_category)
 
         self.assertFalse(response)
--- users/permissions.py
+++ users/permissions.py
@@ -1,9 +1,5 @@
-from rest_framework.permissions import BasePermission
+from common.permissions import IsOwnerOrAdmin
 
 
-class IsSelfOrAdmin(BasePermission):
-    def has_object_permission(self, request, view, obj):
-        return request.user.is_superuser or obj.pk == request.user.pk
-
-    def has_permission(self, request, view):
-        return request.user.is_authenticated
+class IsSelfOrAdmin(IsOwnerOrAdmin):
+    pass
