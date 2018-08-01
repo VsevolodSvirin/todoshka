5cebf1786f9e7f3e66b2a967ea10269842c50a40
VsevolodSvirin <vsvirin@smedialink.com>
2018-08-01 10:03:50 +0300

    Fix permissions for categories

--- categories/permissions.py
+++ categories/permissions.py
@@ -1,11 +1,12 @@
+from rest_framework import permissions
 from rest_framework.permissions import BasePermission
 
 
 class CategoryPermissions(BasePermission):
     def has_object_permission(self, request, view, obj):
-        if view.action == 'destroy':
-            return request.user.is_superuser or not obj.common and obj.user_id and obj.user_id == request.user.pk
-        return request.user.is_superuser or obj.common or obj.user_id and obj.user_id == request.user.pk
+        if request.method in permissions.SAFE_METHODS:
+            return request.user.is_superuser or obj.common or obj.user_id and obj.user_id == request.user.pk
+        return request.user.is_superuser or not obj.common and obj.user_id and obj.user_id == request.user.pk
 
     def has_permission(self, request, view):
         return request.user.is_authenticated
